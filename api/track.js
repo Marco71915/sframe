7cbe7baf5ddcddca8bd84a7a2c2c86c81832d632
Marco71915 <marcoscarpelli05@gmail.com>
2025-08-01 10:26:33 +0200

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,19 +1,10 @@
-export default async function handler(req, res) {
-  const { id } = req.query; // ID del QR code
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+export default function handler(req, res) {
+  const { id } = req.query;
 
-  // Otteniamo il paese dell'utente
-  const geoRes = await fetch(`https://ipapi.co/${ip}/json/`);
-  const geoData = await geoRes.json();
-
-  // Salviamo temporaneamente in memoria (poi useremo un DB)
   if (!global.visits) global.visits = {};
-  if (!global.visits[id]) global.visits[id] = { count: 0, countries: {} };
+  if (!global.visits[id]) global.visits[id] = { count: 0 };
 
   global.visits[id].count++;
-  const country = geoData.country_name || "Unknown";
-  global.visits[id].countries[country] =
-    (global.visits[id].countries[country] || 0) + 1;
 
   res.status(200).json(global.visits[id]);
 }
